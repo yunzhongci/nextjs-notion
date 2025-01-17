984d272e326fc8838aec0662764ffad73564c050
yunzhongci <yunzhongci@icloud.com>
2025-01-17 23:34:53 +0800

    更新配置

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'fde5ac74eea345278f004482710e1af3',
+  rootNotionSpaceId: '',
 
   // basic site info (required)
   name: '白玉京',
