1b848f89952ca947c78bbf9f217b3263e6bc3f7e
yunzhongci <yunzhongci@icloud.com>
2025-01-17 23:40:48 +0800

    更新

--- site.config.ts
+++ site.config.ts
@@ -2,32 +2,32 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '17e965fde97080cf9d5fee034ebb9de1',
+  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: '',
+  rootNotionSpaceId: 'fde5ac74eea345278f004482710e1af3',
 
   // basic site info (required)
-  name: '白玉京',
-  domain: 'wiki.cinas.cn',
-  author: '天上白玉京，十二楼五城',
+  name: 'Transitive Bullshit',
+  domain: 'transitivebullsh.it',
+  author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: '云中辞的个人知识库',
+  description: 'Personal site of Travis Fischer aka Transitive Bullshit',
 
   // social usernames (optional)
-  //twitter: 'transitive_bs',
-  //github: 'transitive-bullshit',
-  //linkedin: 'fisch2',
+  twitter: 'transitive_bs',
+  github: 'transitive-bullshit',
+  linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
-  defaultPageCover: null,
+  defaultPageIcon: 'https://transitivebullsh.it/page-icon.png',
+  defaultPageCover: 'https://transitivebullsh.it/page-cover.jpg',
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
@@ -36,7 +36,7 @@ export default siteConfig({
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: false,
+  isRedisEnabled: true,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
@@ -48,18 +48,21 @@ export default siteConfig({
   // }
   pageUrlOverrides: null,
 
+  pageUrlAdditions: {
+    '/the-social-audio-revolution': 'c4deaf33cc924ad7a5b9f69c6ae04a01'
+  },
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'custom',
   navigationLinks: [
     {
-      title: '关于',
-      pageId: '17e965fde97080f18e5fe6705c7f57c4'
+      title: 'About',
+      pageId: '8d0062776d0c4afca96eb1ace93a7538'
     },
-    //{
-     // title: 'Contact',
-    //  pageId: '9a7ddf2973444067bbc5ce0a4e0e0058'
-    //}
+    {
+      title: 'Contact',
+      pageId: '9a7ddf2973444067bbc5ce0a4e0e0058'
+    }
   ]
 })
