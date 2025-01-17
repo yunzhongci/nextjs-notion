e22e06b8b7677675fd2fc64134249eb503eb7364
yunzhongci <yunzhongci@icloud.com>
2025-01-17 23:01:14 +0800

    transitive-bullshit

---
+++ site.config.ts
@@ -0,0 +1,68 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: 'fde5ac74eea345278f004482710e1af3',
+
+  // basic site info (required)
+  name: 'Transitive Bullshit',
+  domain: 'transitivebullsh.it',
+  author: 'Travis Fischer',
+
+  // open graph metadata (optional)
+  description: 'Personal site of Travis Fischer aka Transitive Bullshit',
+
+  // social usernames (optional)
+  twitter: 'transitive_bs',
+  github: 'transitive-bullshit',
+  linkedin: 'fisch2',
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: 'https://transitivebullsh.it/page-icon.png',
+  defaultPageCover: 'https://transitivebullsh.it/page-cover.jpg',
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: true,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  pageUrlAdditions: {
+    '/the-social-audio-revolution': 'c4deaf33cc924ad7a5b9f69c6ae04a01'
+  },
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '8d0062776d0c4afca96eb1ace93a7538'
+    },
+    {
+      title: 'Contact',
+      pageId: '9a7ddf2973444067bbc5ce0a4e0e0058'
+    }
+  ]
+})
